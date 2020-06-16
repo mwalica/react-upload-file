4002e326c4c10fa238f3fa4a5e5f9bab67f5e8ab
Marek Walica <mwalica@gmail.com>
2020-06-16 13:15:12 +0200

    build and server.js upload

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.post("/upload", (req, res) => {
     return res.status(400).json({ msg: "No file uploaded" });
   }
   const file = req.files.file;
-  file.mv(`${__dirname}/client/public/uploads/${file.name}`, (err) => {
+  file.mv(`${__dirname}/client/build/uploads/${file.name}`, (err) => {
     if (err) {
       console.error(err);
       return res.status(500).send(err);
