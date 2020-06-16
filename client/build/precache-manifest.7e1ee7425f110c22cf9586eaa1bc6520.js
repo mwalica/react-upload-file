05ab201ff070855d9ce6c2cd14ffd977d82f248f
Marek Walica <mwalica@gmail.com>
2020-06-16 13:16:47 +0200

    react build

---
+++ client/build/precache-manifest.7e1ee7425f110c22cf9586eaa1bc6520.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a8ce0c2d2b9dd1092bb56cd02785d58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8502bfe7b33d0985e0ee",
+    "url": "/static/js/2.69a2d200.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.69a2d200.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "098d5581a319c6946f76",
+    "url": "/static/js/main.97a05135.chunk.js"
+  },
+  {
+    "revision": "bbf474368646641dcab4",
+    "url": "/static/js/runtime-main.cdf84635.js"
+  }
+]);
